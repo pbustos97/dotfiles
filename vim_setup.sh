b0d8ee5db197a26972557a77e530a4b9108c9e3a
Patrick Bustos <saintpat97@gmail.com>
2021-12-20 12:40:55 -0800

    fixed copy error

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp vimrc /.vimrc
+cp vimrc ~/.vimrc
 vim +PluginInstall +qall
 python3 ~/.vim/bundle/YouCompleteMe/install.py --all
