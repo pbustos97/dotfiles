92b9c4a5c63bac63e0e789ce53781f6037365eeb
Patrick Bustos <saintpat97@gmail.com>
2026-04-11 17:18:09 -0700

    Updated scripts to be easier to edit
    Signed-off-by: Patrick Bustos <saintpat97@gmail.com>

--- install_scripts/install_yazi_plugins.sh
+++ install_scripts/install_yazi_plugins.sh
@@ -1,5 +1,15 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/install_funcs.sh"
 
-install_yazi_plugin "piper"
-install_yazi_plugin "git"
+PLUGINS_FILE="$SCRIPT_DIR/plugins.yazi.txt"
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+    plugin=$(echo "$line" | awk '{print $1}')
+    repo=$(echo "$line" | awk '{print $2}')
+
+    if [[ -n "$repo" ]]; then
+        install_yazi_plugin "$plugin" "$repo"
+    else
+        install_yazi_plugin "$plugin"
+    fi
+done < "$PLUGINS_FILE"
